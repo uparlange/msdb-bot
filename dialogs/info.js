1253b02f251c78d4cc02f11a831787caadcde81b
parlangeu <ugo.parlange@cgi.com>
2017-01-13 22:25:03 +0100

    manage PUBLIC_URL

--- dialogs/info.js
+++ dialogs/info.js
@@ -8,7 +8,7 @@ module.exports = {
                 .title('L10N_MAME_TITLE')
                 .text('L10N_MAME_DESCRIPTION')
                 .images([
-                    builder.CardImage.create(session, process.env.DEPLOY_URL + ':' + process.env.PORT + '/public/images/msdb_capture.png')
+                    builder.CardImage.create(session, process.env.PUBLIC_URL + '/public/images/msdb_capture.png')
                 ])
                 .buttons([
                     builder.CardAction.openUrl(session, 'https://msdb.lapli.fr', 'L10N_CONSULT')
