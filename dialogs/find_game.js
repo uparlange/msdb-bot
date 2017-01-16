e94d7dbc46525c5714ca5465e5d36106d02f1f62
parlangeu <ugo.parlange@cgi.com>
2017-01-16 05:24:03 +0100

    Add Typing before searching

--- dialogs/find_game.js
+++ dialogs/find_game.js
@@ -1,31 +1,25 @@
 const builder = require('botbuilder');
 const rp = require('request-promise');
 
-const baseUrl = 'https://msdb.lapli.fr/php/services';
+const BASE_URL = 'https://msdb.lapli.fr/php/services';
 
-let token = null;
+const MAX_DISPLAYED_ITEMS = 5;
 
 const getInitUrl = function () {
-    return baseUrl + '/init.php';
+    return BASE_URL + '/init.php';
 };
 
 const getSearchUrl = function (gameName, token) {
-    return baseUrl + '/search.php?params={"description":"' + gameName + '"}&token=' + token
+    return BASE_URL + '/search.php?params={"description":"' + gameName + '"}&token=' + token
 }
 
 const getToken = function (callback) {
-    if (token === null) {
-        rp({
-            uri: getInitUrl(),
-            json: true
-        }).then(function (result) {
-            token = result.data.token;
-            callback(token);
-        });
-    }
-    else {
-        callback(token);
-    }
+    rp({
+        uri: getInitUrl(),
+        json: true
+    }).then(function (result) {
+        callback(result.data.token);
+    });
 }
 
 const findGames = function (gameName, callback) {
@@ -48,18 +42,21 @@ module.exports = {
             builder.Prompts.text(session, 'L10N_PROMPT_GAME_NAME');
         },
         function (session, results) {
+            session.sendTyping();
             findGames(results.response, (games) => {
                 if (games.length > 0) {
                     const cards = [];
                     games.forEach((element, index, array) => {
-                        cards.push(new builder.HeroCard(session)
-                            .title(element.description)
-                            .images([
-                                builder.CardImage.create(session, 'https://msdb.lapli.fr/games/' + element.name + '/titles.png')
-                            ])
-                            .buttons([
-                                builder.CardAction.openUrl(session, 'https://msdb.lapli.fr/#/detail?name=' + element.name, 'L10N_CONSULT')
-                            ]));
+                        if (index < MAX_DISPLAYED_ITEMS) {
+                            cards.push(new builder.HeroCard(session)
+                                .title(element.description)
+                                .images([
+                                    builder.CardImage.create(session, 'https://msdb.lapli.fr/games/' + element.name + '/titles.png')
+                                ])
+                                .buttons([
+                                    builder.CardAction.openUrl(session, 'https://msdb.lapli.fr/#/detail?name=' + element.name, 'L10N_CONSULT')
+                                ]));
+                        }
                     });
                     const msg = new builder.Message(session)
                         .attachmentLayout(builder.AttachmentLayout.carousel)
@@ -68,7 +65,6 @@ module.exports = {
                 }
                 else {
                     session.send('L10N_NO_RESULT');
-
                 }
                 const L10N_YES = session.localizer.gettext(session.preferredLocale(), "L10N_YES");
                 const L10N_NO = session.localizer.gettext(session.preferredLocale(), "L10N_NO");
